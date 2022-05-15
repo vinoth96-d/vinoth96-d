7af6b0c9a9df45eddba01282df0bed80dc43efe2
vinoth96-d <vinothleo96@gmail.com>
2022-05-15 11:49:15 +0530

    deleting
    Signed-off-by: vinoth96-d <vinothleo96@gmail.com>

---
+++ github/src/github/Github_ci_cd.java
@@ -0,0 +1,11 @@
+package github;
+
+public class Github_ci_cd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to github");
+	}
+
+}
